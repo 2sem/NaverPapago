b0fea999baf5f0834475a833aa9e7e2394728230
이영준 <LYJ@yeongjun-iui-macbook-pro.local>
2018-05-01 21:19:16 +0900

    Initial Commit

---
+++ NaverPapago/NaverPapago.h
@@ -0,0 +1,19 @@
+//
+//  NaverPapago.h
+//  NaverPapago
+//
+//  Created by 영준 이 on 2018. 5. 1..
+//  Copyright © 2018년 leesam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NaverPapago.
+FOUNDATION_EXPORT double NaverPapagoVersionNumber;
+
+//! Project version string for NaverPapago.
+FOUNDATION_EXPORT const unsigned char NaverPapagoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NaverPapago/PublicHeader.h>
+
+
